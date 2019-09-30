75705c9363979de90b4663faf01f0f17985e90f2
Éric Larivière <eric.lariviere1999@hotmail.com>
2019-09-30 15:50:56 -0400

    Cours 12

---
+++ Cours12/Assets/Script/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    public void Start()
+    {
+        int numberMusicPlayer = FindObjectsOfType<MusicPlayer>().Length;
+        if (numberMusicPlayer > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Cours12/Assets/Script/Win.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour {
+
+    public AudioClip Rire;
+    private MusicPlayer musicPlayer;
+    // Use this for initialization
+    void Start () {
+        musicPlayer = FindObjectOfType<MusicPlayer>();
+        
+	}
+	public void OnTriggerEnter(Collider gameObject)
+    {
+        musicPlayer.GetComponent<AudioSource>().PlayOneShot(Rire);
+    }
+}
